f6eca1fba62c1687ee0a6f89660fcbbe83f3d8f9
jullan-m <jmlindi@stud.ntnu.no>
2020-02-26 11:19:45 +0100

    12.02

--- Plotting/BeefPlotter.py
+++ Plotting/BeefPlotter.py
@@ -5,19 +5,16 @@ from matplotlib import cm
 import matplotlib.animation as animation
 
 class Plotter:
-    def __init__(self, system, filename = 'untitled', save_fig = False):
-        self.system = system
+    def __init__(self, beef, filename = 'untitled', save_fig = False):
+        self.beef = beef
         self.filename = filename
         self.save_fig = save_fig
-        self.n = system.t / self.system.dt
-
-        self.xv, self.yv = np.meshgrid(system.x, system.y)
+        self.n = beef.t / self.beef.dt
 
     def show_heat_map(self, t):
-
         fig, ax = plt.subplots()
 
-        cs1 = [ax.contourf(self.xv, self.yv, self.system.U[:,:,self.n], 65, cmap = cm.magma)]
+        cs1 = [ax.contourf(self.xv, self.yv, self.beef.U[:,:,self.n], 65, cmap = cm.magma)]
         cbar1 = fig.colorbar(cs1[0], ax=ax, shrink=0.9)
         cbar1.ax.set_ylabel(r'$U(x,y)$', fontsize=14)
         plt.xlabel(r"$x$", fontsize=16)
@@ -53,22 +50,12 @@ class Animation:
         self.fig, self.ax1 = plt.subplots()
 
         # Initialize the line object
-        self.line1, = self.ax1.plot(
-            [], [], lw=1.0, color='g', label=r"$\Psi_R$")
-        self.line2, = self.ax1.plot(
-            [], [], lw=1.0, color='m', label=r"$\Psi_I$")
-        self.line3, = self.ax1.plot(
-            [], [], lw=1.0, color='b', label=r"$|\Psi|^2$")
-        self.line4, = self.ax1.plot([], [], lw=1.0, color='k', label=r"$V(x)$")
-        self.lines = [self.line1, self.line2, self.line3]
-        #   Plots potential (NOT in the same units at all)
-        if self.system.hasPotential:
-            self.line4.set_data(self.system.x, self.system.V/(1.5*self.system.E))
-            self.lines.append(self.line4)
+        self.line, = self.ax1.plot(
+            [], [], lw=1.0, color='g', label=r"")
 
         # Set limits and labels for the axes
-        self.ax1.set_xlim(left=0, right=part.L)
-        self.ax1.set_ylim(bottom=-1, top=1)
+        #self.ax1.set_xlim(left=0, right=)
+        #self.ax1.set_ylim(bottom=-1, top=1)
         self.ax1.grid()
 
         # Actually do the animation
@@ -85,8 +72,7 @@ class Animation:
 
         # Assigning the line object a set of values
         self.lines[0].set_data(self.system.x, self.system.reArrEven)
-        self.lines[1].set_data(self.system.x, self.system.imArrEven)
-        self.lines[2].set_data(self.system.x, self.system.rhoArr)
+
 
         # Uncomment the following line to save a hi-res version of each frame (mind the filenames though, they'll overwrite each other)
         # plt.savefig('test.png',format='png',dpi=600)
