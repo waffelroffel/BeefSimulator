b66f4490004d56f7bce54b6a162fa4ce2da8359f
waffelroffel <waffelroffel@gmail.com>
2020-04-07 19:29:02 +0200

    Setup testing with actions (#1)
    * Create pythonapp.yml
    
    * testing actions
    
    * test
    
    * testing
    
    * testing
    
    * automatic testing donw

---
+++ configs/github_conf.py
@@ -0,0 +1,50 @@
+logging = {
+    "init": False,  # print the configuration of beefsimulator
+    "init_state": False,  # print the initial state of T
+    "stage": False,  # print the different stages of beefsimulator
+    "tn": False,  # print the current time step
+    "Ab": False,  # print the A matrix and b vector
+    "final": False  # print the final state of T
+}
+
+folder = "github"
+
+dh = 0.1
+dt = 0.01
+
+t0 = 0
+tlen = 0.1
+tn = t0+tlen
+
+x0 = 0
+xlen = 1
+xn = x0+xlen
+
+y0 = 0
+ylen = 1
+yn = y0+ylen
+
+z0 = 0
+zlen = 1
+zn = z0+zlen
+
+conf = {
+    "dh": dh,
+    "dt": dt,
+    "t0": t0,
+    "tlen": tlen,
+    "tn": tn,
+    "dims": {
+        "x0": x0,
+        "xlen": xlen,
+        "xn": xn,
+        "y0": y0,
+        "ylen": ylen,
+        "yn": yn,
+        "z0": z0,
+        "zlen": zlen,
+        "zn": zn,
+    },
+    "logging": logging,
+    "folder": folder
+}
