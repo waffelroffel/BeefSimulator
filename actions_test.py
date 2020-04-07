75b066898c167b3a0ac4da6d16fd7b4464dce16e
waffelroffel <waffelroffel@gmail.com>
2020-04-07 19:25:19 +0200

    automatic testing donw

--- actions_test.py
+++ actions_test.py
@@ -1,12 +1,11 @@
 import numpy as np
 from BeefSimulator import BeefSimulator
 from scipy.sparse import diags
-from configs.T_conf import T_conf
-from configs.C_conf import C_conf
-from configs.conf import conf
+from configs.github_T_conf import T_conf
+from configs.github_C_conf import C_conf
+from configs.github_conf import conf
 from auxillary_functions import u_w
 
 if __name__ == "__main__":
     bs = BeefSimulator(conf, T_conf, C_conf)
     bs.solve_all()
-    asd
