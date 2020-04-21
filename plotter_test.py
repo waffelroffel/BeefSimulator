0c9b9759c60dbacd80e69956eddc0daffee73754
jullan-m <jmlindi@stud.ntnu.no>
2020-04-21 09:44:35 +0200

    vmin for temp,cons= 0

--- plotter_test.py
+++ plotter_test.py
@@ -1,6 +1,8 @@
 from Plotting.BeefPlotter import Plotter
+from pathlib import Path
 
 if __name__ == "__main__":
-    bp = Plotter(name="data/conv_oven_test_3")
-    bp.show_heat_map2("C", 10, Z=[0, 0.011, 0.022, 0.033, 0.044, 0.055])
-
+    bp = Plotter(name=Path("data/new_conv_oven_780sec"))
+    bp.save_fig = True
+    for i in range(14):
+        bp.show_heat_map2("C", i * 60, X=[0, 0.015, 0.030, 0.045, 0.055])
