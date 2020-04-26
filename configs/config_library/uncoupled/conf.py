f2f205ba562fc1e0e4bbc67024bd59777af73471
Svein Åmdal <sveiaam@stud.ntnu.no>
2020-04-26 20:05:53 +0200

    Added uncoupled equation configs

---
+++ configs/config_library/uncoupled/conf.py
@@ -0,0 +1,60 @@
+# conf
+# uncoupled
+
+logging = {
+    "init": True,  # print the configuration of beefsimulator
+    "init_state": False,  # print the initial state of T
+    "stage": True,  # print the different stages of beefsimulator
+    "tn": True,  # print the current time step
+    "Ab": False,  # print the A matrix and b vector
+    "final": False  # print the final state of T
+}
+
+dh = 1e-3
+dt = 1e-4
+
+# -1: only the last step is saved
+t_jump = 10000  # Save progress each 10 seconds
+
+t0 = 0
+tlen = 780  # 13 min
+tn = t0+tlen
+
+folder = f"uncoupled_t{tlen}_dt{dt:.2g}_dh{dh:.2g}"
+
+# Actual beef size split symmetrically along xy-axes. Implement symmetric B.C.s
+# to compensate.
+
+x0 = 0
+xlen = 0.047
+xn = x0+xlen
+
+y0 = 0
+ylen = 0.025
+yn = y0+ylen
+
+z0 = 0
+zlen = 0.016
+zn = z0+zlen
+
+conf = {
+    "dh": dh,
+    "dt": dt,
+    "t0": t0,
+    "tlen": tlen,
+    "tn": tn,
+    "dims": {
+        "x0": x0,
+        "xlen": xlen,
+        "xn": xn,
+        "y0": y0,
+        "ylen": ylen,
+        "yn": yn,
+        "z0": z0,
+        "zlen": zlen,
+        "zn": zn,
+    },
+    "logging": logging,
+    "folder": folder,
+    "t_jump": t_jump
+}
