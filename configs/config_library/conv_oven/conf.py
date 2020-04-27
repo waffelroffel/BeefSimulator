810d609e85952a8a2441a39b1f7bb5b7e2ac2716
jullan-m <jmlindi@stud.ntnu.no>
2020-04-27 22:28:07 +0200

    Simulate with positive gamma (at request)

--- configs/config_library/conv_oven/conf.py
+++ configs/config_library/conv_oven/conf.py
@@ -14,13 +14,13 @@ dh = 1e-3
 dt = 1e-4
 
 # -1: only the last step is saved
-t_jump = 10000  # Save progress each 10 seconds
+t_jump = 5000  # Save progress each 5 seconds
 
 t0 = 0
 tlen = 780  # 13 min
 tn = t0+tlen
 
-folder = f"conv_oven_t{tlen}_dt{dt:.2g}_dh{dh:.2g}"
+folder = f"pos-conv_oven_t{tlen}_dt{dt:.2g}_dh{dh:.2g}"
 
 # Actual beef size split symmetrically along xy-axes. Implement symmetric B.C.s
 # to compensate.
